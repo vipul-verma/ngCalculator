29a9bf740f449e56632d7168ec2bd67a1f0c3bae
vipul-verma <vipul.firebase@gmail.com>
2018-02-04 23:27:40 +0530

    version1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, AfterViewInit } from '@angular/core';
 
 @Component({
   selector: 'ca-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'ca';
+export class AppComponent implements AfterViewInit {
+  eqStr = '';
+  isClassVisible = false;
+  constructor(private elRef: ElementRef) {  }
+
+  ngAfterViewInit() {
+    const elem = this.elRef.nativeElement.querySelector('.math-button');
+    console.log(elem);
+
+  }
+
+  equation(e) {
+    const str = e.innerHTML;
+    this.eqStr += str;
+    console.log(this.eqStr);
+  }
+
+  setClassTrue() {
+    this.isClassVisible = true;
+  }
+
+  setClassFalse() {
+    this.isClassVisible = false;
+  }
+
 }
